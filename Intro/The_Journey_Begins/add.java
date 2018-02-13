4b6e7764d7671f8ac51bee62f2b53ac08fc9498a
vinicius3333 <35425928+vinicius3333@users.noreply.github.com>
2018-02-13 18:05:44 -0200

    Update add.java

--- Intro/The_Journey_Begins/add.java
+++ Intro/The_Journey_Begins/add.java
@@ -3,10 +3,10 @@ package CodeFights.Intro.The_Journey_Begins;
 public class add {
 
     public void main(String[] args){
-        System.out.println("A soma de 2 + 2 é: " + add(2,2));
+        System.out.println("A soma de 2 + 2 é: " + add(2,2)); //Test
     }
 
     static int add(int param1, int param2){
-        return param1 + param2;
+        return param1 + param2;  //Retorna o valor da soma////Return the value of the sum//
     }
 }
