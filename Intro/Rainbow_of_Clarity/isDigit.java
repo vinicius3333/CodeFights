5ce7b681db44729d2f2c5974a33e2cb290e719f2
vinicius3333 <35425928+vinicius3333@users.noreply.github.com>
2018-03-12 17:08:04 -0300

    Add files via upload

---
+++ Intro/Rainbow_of_Clarity/isDigit.java
@@ -0,0 +1,11 @@
+package CodeFights.Intro.Rainbow_of_Clarity;
+
+public class isDigit {
+    public static void main(String[] args) {
+        System.out.println(isDigit('-'));
+    }
+
+    static boolean isDigit(char symbol) {
+        return Character.isDigit(symbol);
+    }
+}
