6830faf59cfefcca6b4337c84ed1ec6ba9bb2179
vinicius3333 <35425928+vinicius3333@users.noreply.github.com>
2018-05-01 18:32:55 -0300

    Update alternatingSums.java

--- Intro/Exploring_The_Waters/alternatingSums.java
+++ Intro/Exploring_The_Waters/alternatingSums.java
@@ -9,18 +9,12 @@ public class alternatingSums {
 
     static int[] alternatingSums(int[] a){
         int[] c = new int[2];
-        int d = 0;
 
-        for(int i  = 0; i < a.length; i++){
-            if(d == 0){
-                c[0] += a[i];
-                d++;
-            }
-            else if(d == 1){
-                c[1] += a[i];
-                d = 0;
-            }
+        for(int i = 0; i < a.length; i++){
+            if(i%2==0) c[0] += a[i];
+            else c[1] += a[i];
         }
+
         return c;
     }
 }
