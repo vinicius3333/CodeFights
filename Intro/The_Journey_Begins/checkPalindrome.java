b77e4be66cc1005ca893e9a1f5cb123e75a1ac14
vinicius3333 <35425928+vinicius3333@users.noreply.github.com>
2018-05-03 19:17:22 -0300

    Update checkPalindrome.java

--- Intro/The_Journey_Begins/checkPalindrome.java
+++ Intro/The_Journey_Begins/checkPalindrome.java
@@ -2,15 +2,15 @@ package CodeFights.Intro.The_Journey_Begins;
 
 /*** 
 ---Pergunta/ Question---
-Dada uma string, verifique se ela é um palíndromo.
-*Um palíndromo é uma string que é igual lida tanto da esquerda pra direita quanto da direita pra esquerda.*
-
 Given the string, check if it is a palindrome.
 *A palindrome is a string that reads the same left-to-right and right-to-left.*
----Comentários/ Comments---
-Apenas crie um algoritmo que verifique se a string é igual a ela reverse.
 
+Dada uma string, verifique se ela é um palíndromo.
+*Um palíndromo é uma string que é igual lida tanto da esquerda pra direita quanto da direita pra esquerda.*
+---Comentários/ Comments---
 Just create an algorithm that checks that the string is equal to it's reverse.
+
+Apenas crie um algoritmo que verifique se a string é igual a ela reverse.
 ***/
 
 public class checkPalindrome {
