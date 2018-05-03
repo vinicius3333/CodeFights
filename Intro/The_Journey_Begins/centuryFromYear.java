1ee762b7020363eaac27a3fa425f8c19b59e603c
vinicius3333 <35425928+vinicius3333@users.noreply.github.com>
2018-05-03 19:16:21 -0300

    Update centuryFromYear.java

--- Intro/The_Journey_Begins/centuryFromYear.java
+++ Intro/The_Journey_Begins/centuryFromYear.java
@@ -1,5 +1,18 @@
 package CodeFights.Intro.The_Journey_Begins;
 
+/*** 
+---Pergunta/ Question---
+Given a year, return the century it is in. The first century spans from the year 1
+up to and including the year 100, the second - from the year 101 up to and including the year 200, etc.
+
+Dado um ano, retorne o século em que se encontra. O primeiro século se estende desde o ano 1
+até e incluindo o ano 100, o segundo - do ano 101 até e incluindo o ano 200, etc.
+---Comentários/ Comments---
+Just an algorithm that returns the century of the year.
+
+Apenas escreva um algoritmo que retorne o século do respectivo ano.
+***/
+
 public class centuryFromYear {
 
     public static void main(String[] args){
@@ -7,12 +20,6 @@ public class centuryFromYear {
     }
 
     static int centuryFromYear(int year){
-        if(year%100 != 0){            //Verifica se o ano não é um século exato, se ele realmente não for |
-            year += 100 - (year%100); //soma o restante para formar um século exato e retorna o século    |
-            return year/100;          //Exemplo: year: 56| 56 != 0 --- 56 += 100 - 56 --- 56 += 44        |  
-        }                             //retorna 1 que é o século que 56 está situado.                     |
-        else return year/100;         //==================================================================|
-    }                                 // Verify that the year is not an exact century, if it really is not|
-}                                     // sum the rest to form an exact century and the century returns    |
-                                      // Example: year: 56 | 56! = 0 --- 56 + = 100 - 56 --- 56 + = 44    |
-                                      // returns 1 which is the century that 56 is situated. 			  |
+        return year%100 != 0 ? (year+100)/100 : year/100;    
+    }                                 
+}                                     
