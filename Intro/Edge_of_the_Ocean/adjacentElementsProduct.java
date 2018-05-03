58bf8852e3641c6088020d84d11398bb5c910df8
vinicius3333 <35425928+vinicius3333@users.noreply.github.com>
2018-05-03 19:35:47 -0300

    Update adjacentElementsProduct.java

--- Intro/Edge_of_the_Ocean/adjacentElementsProduct.java
+++ Intro/Edge_of_the_Ocean/adjacentElementsProduct.java
@@ -1,18 +1,29 @@
 package CodeFights.Intro.Edge_of_the_Ocean;
 
+/*** 
+---Pergunta/ Question---
+Given an array of integers, find the pair of adjacent elements that has the largest product and return that product.
+
+Dada uma matriz de números inteiros, encontre o par de elementos adjacentes que possui o maior produto e retorne esse produto.
+---Comentários/ Comments---
+Make an algorithm to find the largest product among the adjacent elements of an array.
+
+Faça um algoritmo para encontrar o maior produto entre os elementos adjacentes de uma array.
+***/
+
 public class adjacentElementsProduct {
+    
     public static void main(String[] args){
         System.out.println(adjacentElementProduct(new int[]{-23, 4, -3, 8, -12}));
     }
 
-    static int adjacentElementProduct(int[] inputArray){
-        int max = -1000;
+    static int adjacentElementsProduct(int[] inputArray) {
+        int prod = inputArray[0] * inputArray[1];
 
-        for(int i = 0; i < inputArray.length; i++){
-            for(int j = 0; j < inputArray.length; j++){
-                if(inputArray[i] * inputArray[j] > max && Math.abs(i - j) == 1) max = inputArray[i] * inputArray[j];
-            }
+        for(int i = 1; i < inputArray.length-1; i++){
+                int temp = inputArray[i] * inputArray[i+1];
+                if(temp > prod) prod = temp;
         }
-        return max;
+        return prod;
     }
 }
