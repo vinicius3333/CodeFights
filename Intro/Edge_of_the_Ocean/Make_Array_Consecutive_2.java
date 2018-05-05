fd664919cd7f6babf88c3dcef96febbb2e06efde
vinicius3333 <35425928+vinicius3333@users.noreply.github.com>
2018-05-05 19:48:06 -0300

    Update Make_Array_Consecutive_2.java

--- Intro/Edge_of_the_Ocean/Make_Array_Consecutive_2.java
+++ Intro/Edge_of_the_Ocean/Make_Array_Consecutive_2.java
@@ -1,5 +1,26 @@
 package CodeFights.Intro.Edge_of_the_Ocean;
 
+/*** 
+---Pergunta/ Question---
+Ratiorg got statues of different sizes as a present
+from CodeMaster for his birthday, each statue having an non-negative integer size. 
+Since he likes to make things perfect, he wants to arrange them from smallest to largest
+so that each statue will be bigger than the previous one exactly by 1. 
+He may need some additional statues to be able to accomplish that. 
+Help him figure out the minimum number of additional statues needed.
+
+Ratiorg obteve estátuas de tamanhos diferentes como um presente
+do CodeMaster para seu aniversário, cada estátua tendo um tamanho inteiro não negativo.
+Desde que ele gosta de fazer as coisas perfeitas, ele quer organizá-las do menor para o maior
+para que cada estátua seja maior que a anterior exatamente por 1.
+Eu posso precisar de algumas estátuas adicionais para conseguir isso.
+Ajude-o a descobrir o número mínimo de estátuas adicionais necessárias.
+---Comentários/ Comments---
+Make an algorithm that checks how many statues are missing from the beginning to the end of the array.
+
+Faça um algoritmo que verifique quantas estátuas estão faltando desde o início até o final da array.
+***/
+
 import java.util.Collections;
 import java.util.HashSet;
 
