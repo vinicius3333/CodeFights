f9dacde76e6c5760d2828739032cda82241e48eb
vinicius3333 <35425928+vinicius3333@users.noreply.github.com>
2018-05-19 20:41:56 -0300

    Add files via upload

---
+++ The_Core/List_Forest_Edge/Make_Array_Consecutive_2.java
@@ -0,0 +1,26 @@
+package CodeFights.The_Core.List_Forest_Edge;
+
+import java.util.Collections;
+import java.util.HashSet;
+
+public class Make_Array_Consecutive_2 {
+    public static void main(String[] args) {
+        System.out.println(makeArrayConsecutive2(new int[]{6,2,3,8}));
+    }
+
+    static int makeArrayConsecutive2(int[] statues) {
+        int c = 0;
+        HashSet<Integer> set = new HashSet<>();
+        for(int i = 0; i < statues.length; i++){
+            set.add(statues[i]);
+        }
+        int min = Collections.min(set);
+        int max = Collections.max(set);
+
+        for(int i = min; i < max; i++){
+            if(!set.contains(i)) c++;
+        }
+
+        return c;
+    }
+}
