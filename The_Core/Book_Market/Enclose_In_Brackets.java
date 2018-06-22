70a92462d0250c51fa17e937e6ee1f5e8d6eb4b0
vinicius3333 <35425928+vinicius3333@users.noreply.github.com>
2018-06-22 17:17:01 -0300

    Add files via upload

---
+++ The_Core/Book_Market/Enclose_In_Brackets.java
@@ -0,0 +1,11 @@
+package CodeFights.The_Core.Book_Market;
+
+public Enclose_in_Brackets {
+    public static void main(String[] args){
+        System.out.println();
+    }
+
+    static String encloseInBrackets(String inputString) {
+        return "(" + inputString + ")";
+    }
+}
