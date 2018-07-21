8ee27a49dd3a21081bc9223b1d56422bedd3b6f7
vinicius3333 <35425928+vinicius3333@users.noreply.github.com>
2018-07-20 22:36:41 -0300

    Update matrixElementsSum.java

--- Intro/Edge_of_the_Ocean/matrixElementsSum.java
+++ Intro/Edge_of_the_Ocean/matrixElementsSum.java
@@ -1,5 +1,28 @@
 package CodeFights.Intro.Edge_of_the_Ocean;
 
+/*** 
+---Pergunta/ Question---
+After they became famous, the CodeBots all decided to move to a new building and live together. 
+The building is represented by a rectangular matrix of rooms. Each cell in the matrix contains 
+an integer that represents the price of the room. Some rooms are free (their cost is 0), but that's 
+probably because they are haunted, so all the bots are afraid of them. That is why any room that is 
+free or is located anywhere below a free room in the same column is not considered suitable for the
+bots to live in.
+Help the bots calculate the total price of all the rooms that are suitable for them.
+
+Depois que ficaram famosos, os CodeBots decidiram mudar para um novo prédio e morar juntos. 
+O edifício é representado por uma matriz retangular de salas. Cada célula na matriz contém 
+um inteiro que representa o preço da sala. Alguns quartos são gratuitos (seu custo é 0), mas
+isso é provavelmente porque eles são assombrados, então todos os bots têm medo deles. É por 
+isso que qualquer sala que é gratuita ou está localizada em qualquer lugar abaixo de uma sala
+livre na mesma coluna não é considerada adequada para os robôs viverem.
+Ajude os bots a calcular o preço total de todos os quartos que são adequados para eles.
+---Comentários/ Comments---
+Make an algorithm that calculates the elements of an array, but the values ​​below the number 0 must not be added
+
+Fazer um algoritmo que calcule os elementos de uma matriz, contudo os valores em baixo do numero 0 não deve ser somado
+***/
+
 public class matrixElementsSum {
     public static void main(String[] args){
         System.out.println(matrixElementsSum(new int[][]{new int[]{1, 1, 1, 0},
