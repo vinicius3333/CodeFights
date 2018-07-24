cba09be66bd29dc1ed426251f9b7c364e919e802
vinicius3333 <35425928+vinicius3333@users.noreply.github.com>
2018-07-23 21:58:03 -0300

    Update isLucky.java

--- Intro/Smooth_Sailing/isLucky.java
+++ Intro/Smooth_Sailing/isLucky.java
@@ -1,5 +1,20 @@
 package CodeFights.Intro.Smooth_Sailing;
 
+/*** 
+---Pergunta/ Question---
+Ticket numbers usually consist of an even number of digits. A ticket number is considered 
+lucky if the sum of the first half of the digits is equal to the sum of the second half.
+Given a ticket number n, determine if it's lucky or not.
+
+Geralmente, os números dos tickets consistem em um número par de dígitos. Um número de bilhete
+é considerado sortudo se a soma da primeira metade dos dígitos for igual à soma da segunda parte.
+Dado um número de ticket n, determine se ele é sortudo ou não.
+---Comentários/ Comments---
+Make an algorithm that compares the sum of the first half and the second part
+
+Faça um algoritmo que compare a soma da primeira metade e a segunda parte
+***/
+
 import java.util.stream.IntStream;
 
 public class isLucky {
