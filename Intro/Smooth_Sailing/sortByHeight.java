0f17cd85f5b828ecbee932270a70dbf345a3f058
vinicius3333 <35425928+vinicius3333@users.noreply.github.com>
2018-07-24 22:49:18 -0300

    Update sortByHeight.java

--- Intro/Smooth_Sailing/sortByHeight.java
+++ Intro/Smooth_Sailing/sortByHeight.java
@@ -1,5 +1,22 @@
 package CodeFights.Intro.Smooth_Sailing;
 
+/*** 
+---Pergunta/ Question---
+Some people are standing in a row in a park. There are trees between 
+them which cannot be moved. Your task is to rearrange the people by 
+their heights in a non-descending order without moving the trees. 
+People can be very tall!
+
+Algumas pessoas estão em fila em um parque. Há árvores entre elas
+que não podem ser movidas. Sua tarefa é reorganizar as pessoas por 
+suas alturas em uma ordem não descendente sem mover as árvores. As 
+pessoas podem ser muito altas!
+---Comentários/ Comments---
+Make an algorithm to put an array in ascending order, not changing the position of -1
+
+Faça um algoritmo para colocar em ordem crescente um array, não mudando a posição do -1
+***/
+
 public class sortByHeight {
     public static void main(String[] args){
         System.out.println(sortByHeight(new int[]{1,3,2,4,-1,6,8,3,5}));
