d7efefa795bc5615156be4a1ff54d90a149b6077
vinicius3333 <35425928+vinicius3333@users.noreply.github.com>
2018-07-25 22:31:36 -0300

    Update reverseParentheses.java

--- Intro/Smooth_Sailing/reverseParentheses.java
+++ Intro/Smooth_Sailing/reverseParentheses.java
@@ -1,5 +1,22 @@
 package CodeFights.Intro.Smooth_Sailing;
 
+/*** 
+---Pergunta/ Question---
+You have a string s that consists of English letters, punctuation marks, whitespace characters,
+and brackets. It is guaranteed that the parentheses in s form a regular bracket sequence.
+Your task is to reverse the strings contained in each pair of matching parentheses, starting 
+from the innermost pair. The results string should not contain any parentheses.
+
+Você tem uma string s que consiste em letras inglesas, sinais de pontuação, caracteres de espaço 
+em branco e colchetes. É garantido que os parênteses em s formam uma seqüência de colchetes regular.
+Sua tarefa é reverter as strings contidas em cada par de parênteses correspondentes, começando 
+pelo par mais interno. A string de resultados não deve conter parênteses.
+---Comentários/ Comments---
+Make an algorithm that inverts the letters within a parenthesis
+
+Faça um algoritmo que inverta as letras dentro de um parêntese
+***/
+
 public class reverseParentheses {
     public static void main(String[] args){
         System.out.println(reverseParentheses("a(bcdefghijkl(mno)p)q"));
