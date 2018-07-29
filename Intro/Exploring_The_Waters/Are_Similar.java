4360080418992ca8d261464a630723ec8e33ed3a
vinicius3333 <35425928+vinicius3333@users.noreply.github.com>
2018-07-28 22:48:34 -0300

    Update Are_Similar.java

--- Intro/Exploring_The_Waters/Are_Similar.java
+++ Intro/Exploring_The_Waters/Are_Similar.java
@@ -1,5 +1,18 @@
 package CodeFights.Intro.Exploring_The_Waters;
 
+/*** 
+---Pergunta/ Question---
+Two arrays are called similar if one can be obtained from another by swapping at most one pair of elements in one of the arrays.
+Given two arrays a and b, check whether they are similar.
+
+Dois arrays são chamados semelhantes se um pode ser obtido de outro, trocando no máximo um par de elementos em um dos arrays.
+Dados dois arrays aeb, verifique se eles são semelhantes.
+---Comentários/ Comments---
+Make an algorithm that shows if the two arrays have similar numbers
+
+Faça um algoritmo que mostre se as duas arrays tem numeros similares
+***/
+
 import java.util.ArrayList;
 import java.util.Collections;
 
