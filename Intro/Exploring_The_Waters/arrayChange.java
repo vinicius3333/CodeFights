cbab0ad171c3a38eb852a208d38256e3e2b5b587
vinicius3333 <35425928+vinicius3333@users.noreply.github.com>
2018-07-30 22:52:29 -0300

    Update arrayChange.java

--- Intro/Exploring_The_Waters/arrayChange.java
+++ Intro/Exploring_The_Waters/arrayChange.java
@@ -1,12 +1,23 @@
 package CodeFights.Intro.Exploring_The_Waters;
 
+/*** 
+---Pergunta/ Question---
+You are given an array of integers. On each move you are allowed to increase exactly one of its element by one. 
+Find the minimal number of moves required to obtain a strictly increasing sequence from the input.
+
+Você recebe uma matriz de inteiros. Em cada movimento, você tem permissão para aumentar exatamente um de seus elementos em um.
+Encontre o número mínimo de movimentos necessários para obter uma sequência estritamente crescente da entrada.
+---Comentários/ Comments---
+Make an algorithm that leaves the array in ascending order
+
+Faça um algoritmo que deixe a array em ordem crescente
+***/
+
 public class arrayChange {
     public static void main(String[] args){
         System.out.println(arrayChange(new int[]{1,1,1}));
     }
-    /***
-    ALGORITMO PARA RETORNAR A SOMA DE TODAS AS ALTERAÇÕES DE UMA ARRAY
-    ***/
+    
     static int arrayChange(int[] inputArray){
         int d = 0;
         for(int i = 0; i < inputArray.length-1; i++){
