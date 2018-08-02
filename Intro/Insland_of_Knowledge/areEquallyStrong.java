adcbe12ee08082e76edae832217bdcf66df9ca95
vinicius3333 <35425928+vinicius3333@users.noreply.github.com>
2018-08-01 22:43:30 -0300

    Update areEquallyStrong.java

--- Intro/Insland_of_Knowledge/areEquallyStrong.java
+++ Intro/Insland_of_Knowledge/areEquallyStrong.java
@@ -1,5 +1,21 @@
 package CodeFights.Intro.Insland_of_Knowledge;
 
+/*** 
+---Pergunta/ Question---
+Call two arms equally strong if the heaviest weights they each are able to lift are equal.
+Call two people equally strong if their strongest arms are equally strong (the strongest arm can be both the right and the left),
+and so are their weakest arms. Given your and your friend's arms' lifting capabilities find out if you two are equally strong.
+
+Chame dois braços igualmente fortes se os pesos mais pesados ​​que cada um deles conseguir levantar forem iguais.
+Chame duas pessoas igualmente fortes se seus braços mais fortes forem igualmente fortes 
+(o braço mais forte pode ser tanto o direito quanto o esquerdo), assim como os braços mais fracos. Dada a capacidade
+de levantamento de seus braços e do seu amigo, descubra se vocês dois são igualmente fortes.
+---Comentários/ Comments---
+Make an algorithm that checks whether the two pairs of numbers are equivalent
+
+Faça um algoritmo que verifique se os dois pares de números são equivalentes
+***/
+
 public class areEquallyStrong {
     public static void main(String[] args){
         System.out.println(areEquallyStrong(15,10,15,9));
