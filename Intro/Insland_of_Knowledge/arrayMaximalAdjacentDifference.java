31f98db0ee81d2343bbc8952429542cebe7cecd4
vinicius3333 <35425928+vinicius3333@users.noreply.github.com>
2018-08-02 08:36:43 -0300

    Update arrayMaximalAdjacentDifference.java

--- Intro/Insland_of_Knowledge/arrayMaximalAdjacentDifference.java
+++ Intro/Insland_of_Knowledge/arrayMaximalAdjacentDifference.java
@@ -1,5 +1,16 @@
 package CodeFights.Intro.Insland_of_Knowledge;
 
+/*** 
+---Pergunta/ Question---
+Given an array of integers, find the maximal absolute difference between any two of its adjacent elements.
+
+Dada uma matriz de inteiros, encontre a diferença absoluta máxima entre quaisquer dois dos seus elementos adjacentes.
+---Comentários/ Comments---
+Make an algorithm that returns the greatest value of the difference of the adjacent numbers of an Array
+
+Faça um algoritmo que retorne o maior valor da diferença dos números adjacentes de uma Array
+***/
+
 public class arrayMaximalAdjacentDifference {
     public static void main(String[] args){
         System.out.println(arrayMaximalAdjacentDifference(new int[]{-1, 4, 10, 3, -2}));
