44842a88efd9285c4343244fbe6a1bf7d0528f52
vinicius3333 <35425928+vinicius3333@users.noreply.github.com>
2018-08-05 19:36:45 -0300

    Update Box_Blur.java

--- Intro/Insland_of_Knowledge/Box_Blur.java
+++ Intro/Insland_of_Knowledge/Box_Blur.java
@@ -1,5 +1,24 @@
 package CodeFights.Intro.Insland_of_Knowledge;
 
+/*** 
+---Pergunta/ Question---
+Last night you partied a little too hard. Now there's a black and white photo of you that's about to go viral! You can't let 
+this ruin your reputation, so you want to apply the box blur algorithm to the photo to hide its content.
+The pixels in the input image are represented as integers. The algorithm distorts the input image in the following way: Every 
+pixel x in the output image has a value equal to the average value of the pixel values from the 3 × 3 square that has its center at x, including x itself. All the pixels on the border of x are then removed.
+Return the blurred image as an integer, with the fractions rounded down.
+
+Ontem à noite você festejou um pouco demais. Agora há uma foto em preto e branco de você que está prestes a se tornar viral! Você não pode deixar
+isso arruína sua reputação, portanto, você deseja aplicar o algoritmo de desfoque de caixa à foto para ocultar seu conteúdo.
+Os pixels na imagem de entrada são representados como inteiros. O algoritmo distorce a imagem de entrada da seguinte maneira:
+pixel x na imagem de saída tem um valor igual ao valor médio dos valores de pixel do quadrado 3 × 3 que tem seu centro em x, incluindo x propriamente dito. Todos os pixels na borda de x são removidos.
+Retorna a imagem borrada como um inteiro, com as frações arredondadas para baixo.
+---Comentários/ Comments---
+Make an algorithm that adds the numbers of a 3 x 3 area of ​​an array and returns a new array with the new sum
+
+Faça um algoritmo que some os números de uma area 3 x 3 de uma matriz e retorne uma nova matriz com a nova soma
+***/
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
