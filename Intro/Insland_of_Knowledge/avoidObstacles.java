9f9c69acb431223d0119224a8ca6bbf62cb9620e
vinicius3333 <35425928+vinicius3333@users.noreply.github.com>
2018-08-04 22:39:55 -0300

    Update avoidObstacles.java

--- Intro/Insland_of_Knowledge/avoidObstacles.java
+++ Intro/Insland_of_Knowledge/avoidObstacles.java
@@ -1,5 +1,20 @@
 package CodeFights.Intro.Insland_of_Knowledge;
 
+/*** 
+---Pergunta/ Question---
+You are given an array of integers representing coordinates of obstacles situated on a straight line.
+Assume that you are jumping from the point with coordinate 0 to the right. You are allowed only to make 
+jumps of the same length represented by some integer. Find the minimal length of the jump enough to avoid all the obstacles.
+
+Você recebe uma matriz de inteiros representando coordenadas de obstáculos situados em uma linha reta.
+Suponha que você esteja pulando do ponto com a coordenada 0 para a direita. Você só tem permissão para fazer saltos do mesmo 
+comprimento representados por algum inteiro. Encontre o comprimento mínimo do salto o suficiente para evitar todos os obstáculos.
+---Comentários/ Comments---
+Make an algorithm that finds the smallest number to bypass all obstacles
+
+Faça um algoritmo que encontre o menor número para desviar de todos os obstáculos
+***/
+
 import java.util.ArrayList;
 import java.util.Collections;
 
