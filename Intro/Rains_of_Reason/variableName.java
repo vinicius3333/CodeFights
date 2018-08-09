d8a80b321a42d058cf8237dc7c1dcc9375a25290
vinicius3333 <35425928+vinicius3333@users.noreply.github.com>
2018-08-09 18:50:42 -0300

    Update variableName.java

--- Intro/Rains_of_Reason/variableName.java
+++ Intro/Rains_of_Reason/variableName.java
@@ -1,5 +1,18 @@
 package CodeFights.Intro.Rains_of_Reason;
 
+/*** 
+---Pergunta/ Question---
+Correct variable names consist only of English letters, digits and underscores and they can't start with a digit.
+Check if the given string is a correct variable name.
+
+Os nomes das variáveis corretas consistem apenas em letras inglesas, dígitos e sublinhados e não podem começar com um dígito.
+Verifique se a string fornecida é um nome de variável correto.
+---Comentários/ Comments---
+Make an algorithm to check if a String is a suitable name for a variable
+
+Fazer  um algoritmo para verificar se uma String é um nome adequado a uma variável
+***/
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
