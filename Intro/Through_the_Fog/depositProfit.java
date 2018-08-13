3dba7aaa75943f1d841f0256c41e9547f57e9c1f
vinicius3333 <35425928+vinicius3333@users.noreply.github.com>
2018-08-13 15:51:53 -0300

    Update depositProfit.java

--- Intro/Through_the_Fog/depositProfit.java
+++ Intro/Through_the_Fog/depositProfit.java
@@ -1,5 +1,20 @@
 package CodeFights.Intro.Through_the_Fog;
 
+/*** 
+---Pergunta/ Question---
+You have deposited a specific amount of money into your bank account. Each year your balance increases at the same growth rate.
+With the assumption that you don't make any additional deposits, find out how long it would take for your balance to pass a 
+specific threshold. Of course you don't want to wait too long, so you know that the answer is not greater than 6.
+
+Você depositou uma quantia específica em sua conta bancária. A cada ano, seu saldo aumenta na mesma taxa de crescimento.
+Com a suposição de que você não faz nenhum depósito adicional, descubra quanto tempo levaria para que seu saldo passasse
+limiar específico. Claro que você não quer esperar muito, então você sabe que a resposta não é maior que 6.
+---Comentários/ Comments---
+Make an algorithm that shows how many years your deposit will go over the limit
+
+Faça um algoritmo que mostre quantos anos o seu depósito ira passar o limite
+***/
+
 public class depositProfit {
     public static void main(String[] args){
         System.out.print(depositProfit(100,20,170));
