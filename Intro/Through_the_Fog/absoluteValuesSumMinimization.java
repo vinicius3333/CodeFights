1791cd6d711a393bb807d90309bdfa698a5a237b
vinicius3333 <35425928+vinicius3333@users.noreply.github.com>
2018-08-14 08:40:48 -0300

    Update absoluteValuesSumMinimization.java

--- Intro/Through_the_Fog/absoluteValuesSumMinimization.java
+++ Intro/Through_the_Fog/absoluteValuesSumMinimization.java
@@ -1,5 +1,20 @@
 package CodeFights.Intro.Through_the_Fog;
 
+/*** 
+---Pergunta/ Question---
+Given a sorted array of integers a, find an integer x from a such that the value of
+abs(a[0] - x) + abs(a[1] - x) + ... + abs(a[a.length - 1] - x) is the smallest possible (here abs denotes the absolute value).
+If there are several possible answers, output the smallest one.
+
+Dada uma matriz ordenada de inteiros a, encontre um inteiro x de tal que o valor de
+abs (a [0] - x) + abs (a [1] - x) + ... + abs (a [a.length - 1] - x) é o menor possível (aqui abs indica o valor absoluto).
+Se houver várias respostas possíveis, imprima a menor.
+---Comentários/ Comments---
+Make an algorithm that finds the value of x within array a.
+
+Faça um algoritmo que encontre o valor de x dentro da array a
+***/
+
 public class absoluteValuesSumMinimization {
     public static void main(String[] args){
         System.out.print(absoluteValuesSumMinimization(new int[]{2,4,5,7}));
