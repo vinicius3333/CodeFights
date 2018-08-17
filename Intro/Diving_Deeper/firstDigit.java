8f9369f777a533576e21899a0f7dd0b3c5ce95aa
vinicius3333 <35425928+vinicius3333@users.noreply.github.com>
2018-08-17 08:04:32 -0300

    Update firstDigit.java

--- Intro/Diving_Deeper/firstDigit.java
+++ Intro/Diving_Deeper/firstDigit.java
@@ -1,5 +1,16 @@
 package CodeFights.Intro.Diving_Deeper;
 
+/*** 
+---Pergunta/ Question---
+Find the leftmost digit that occurs in a given string.
+
+Encontre o dígito mais à esquerda que ocorre em uma determinada string.
+---Comentários/ Comments---
+Make an algorithm to find the leftmost number of the String
+
+Faça um algoritmo para encontrar o número mais a esquerda possível da String
+***/
+
 public class firstDigit {
     public static void main(String[] args) {
         System.out.println(firstDigit("var_1__Int"));
