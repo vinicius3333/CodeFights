a814e0a2833f46820d0a4c05d8687f9430638745
vinicius3333 <35425928+vinicius3333@users.noreply.github.com>
2018-08-19 21:18:24 -0300

    Update arrayMaxConsecutiveSum.java

--- Intro/Diving_Deeper/arrayMaxConsecutiveSum.java
+++ Intro/Diving_Deeper/arrayMaxConsecutiveSum.java
@@ -1,5 +1,16 @@
 package CodeFights.Intro.Diving_Deeper;
 
+/*** 
+---Pergunta/ Question---
+Given array of integers, find the maximal possible sum of some of its k consecutive elements.
+
+Dada matriz de inteiros, encontre a soma máxima possível de alguns dos seus k elementos consecutivos.
+---Comentários/ Comments---
+Make an algorithm to find the largest sum of two numbers in an array
+
+Faça um algoritmo para descobrir a maior soma de dois números de uma array
+***/
+
 public class arrayMaxConsecutiveSum {
     public static void main(String[] args) {
         System.out.println(arrayMaxConsecutiveSum(new int[]{1,3,2,4}, 3));
