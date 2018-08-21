c0ab92b2fcdb3c418dcbcd909056a0bc858de295
vinicius3333 <35425928+vinicius3333@users.noreply.github.com>
2018-08-21 17:09:36 -0300

    Update KnapsackLight.java

--- Intro/Dark_Wilderness/KnapsackLight.java
+++ Intro/Dark_Wilderness/KnapsackLight.java
@@ -1,5 +1,23 @@
 package codeFighters;
 
+/*** 
+---Pergunta/ Question---
+You found two items in a treasure chest! The first item weighs weight1 and is worth value1, and the second item weighs weight2 
+and is worth value2. What is the total maximum value of the items you can take with you, assuming that your max weight capacity 
+is maxW and you can't come back for the items later? Note that there are only two items and you can't bring more than one item 
+of each type, i.e. you can't take two first items or two second items.
+
+Você encontrou dois itens em um baú de tesouro! O primeiro item pesa o peso1 e vale a pena valor1, e o segundo item pesa o peso2
+e vale a pena value2. Qual é o valor máximo total dos itens que você pode levar com você, supondo que sua capacidade máxima de peso
+é maxW e você não pode voltar para os itens mais tarde? Note que existem apenas dois itens e você não pode trazer mais de um item
+de cada tipo, ou seja, você não pode levar dois primeiros itens ou dois segundos itens.
+---Comentários/ Comments---
+Make an algorithm you discover according to the weight of each treasure, and also your own limit, the amount you can carry.
+
+Faça um algoritmo que descubra de acordo com o peso de cada tesouro, e também o seu próprio limite, o valor que você consegue
+carregar.
+***/
+
 public class KnapsackLight {
 	public static void main(String[] args) {
 		System.out.println(knapsackLight(10,5,6,4,8));
