da9bf1f0f4a4dcb6ef8855cc18a4f2632aec746d
vinicius3333 <35425928+vinicius3333@users.noreply.github.com>
2018-08-23 08:27:10 -0300

    Update DigitDegree.java

--- Intro/Dark_Wilderness/DigitDegree.java
+++ Intro/Dark_Wilderness/DigitDegree.java
@@ -1,5 +1,18 @@
 package codeFighters;
 
+/*** 
+---Pergunta/ Question---
+Let's define digit degree of some positive integer as the number of times we need to replace this number with the sum of its
+digits until we get to a one digit number. Given an integer, find its digit degree.
+
+Vamos definir o grau de dígito de um número inteiro positivo como o número de vezes que precisamos substituir esse número pela 
+soma de seus dígitos até chegarmos a um número de um dígito. Dado um inteiro, encontre seu grau de dígito.
+---Comentários/ Comments---
+Make an algorithm that counts how many times we need to add the digits of the number until it is in a single digit.
+
+Faça um algoritmo que conte quantas vezes precisamos somar os digítos do número até ele ficar com um só digíto.
+***/
+
 public class DigitDegree {
 	public static void main(String[] args) {
 		System.out.println(digitDegree(91));
