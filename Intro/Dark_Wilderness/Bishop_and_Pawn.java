4f35f8c539a57557a834f0e1e3422a0b548eb3cd
vinicius3333 <35425928+vinicius3333@users.noreply.github.com>
2018-08-24 22:57:50 -0300

    Update Bishop_and_Pawn.java

--- Intro/Dark_Wilderness/Bishop_and_Pawn.java
+++ Intro/Dark_Wilderness/Bishop_and_Pawn.java
@@ -1,5 +1,19 @@
 package CodeFights.Intro.Dark_Wilderness;
 
+
+/*** 
+---Pergunta/ Question---
+Given the positions of a white bishop and a black pawn on the standard chess board, determine whether the bishop can capture the
+pawn in one move. The bishop has no restrictions in distance for each move, but is limited to diagonal movement.
+
+Dadas as posições de um bispo branco e um peão preto no tabuleiro de xadrez padrão, determine se o bispo pode capturar o
+peão em um movimento. O bispo não tem restrições de distância para cada movimento, mas se limita ao movimento diagonal.
+---Comentários/ Comments---
+Make an algorithm to find out if in the position of the bishop in the game he can hit his target.
+
+Faça um algoritmo para descobrir se na posição do bispo no jogo ele consegue acertar o seu alvo.
+***/
+
 public class Bishop_and_Pawn {
     public static void main(String[] args) {
         System.out.println(bishopAndPawn("e3","a7"));
