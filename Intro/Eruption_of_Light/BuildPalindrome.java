fa394ea30d37373fcdd93aa5ea2590c211816a8f
vinicius3333 <35425928+vinicius3333@users.noreply.github.com>
2018-08-27 18:43:47 -0300

    Update BuildPalindrome.java

--- Intro/Eruption_of_Light/BuildPalindrome.java
+++ Intro/Eruption_of_Light/BuildPalindrome.java
@@ -1,5 +1,18 @@
 package CodeFights.Intro.Eruption_of_Light;
 
+/*** 
+---Pergunta/ Question---
+Given a string, find the shortest possible string which can be achieved by adding characters to the end of initial string to make
+it a palindrome.
+
+Dada uma string, encontre a string mais curta possível que pode ser obtida adicionando caracteres ao final da string inicial para
+torná-la um palíndromo.
+---Comentários/ Comments---
+Make an algorithm to create a palindrome with the given String.
+
+Faça um algoritmo para criar um palíndromo com a String dada.
+***/
+
 public class BuildPalindrome {
     public static void main(String[] args) {
         System.out.println(buildPalindrome("abcdc"));
