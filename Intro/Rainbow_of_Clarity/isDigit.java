86e8a6156b0e0c4dfde2fb1a2ae9361a105414ae
vinicius3333 <35425928+vinicius3333@users.noreply.github.com>
2018-08-30 20:31:30 -0300

    Update isDigit.java

--- Intro/Rainbow_of_Clarity/isDigit.java
+++ Intro/Rainbow_of_Clarity/isDigit.java
@@ -1,5 +1,16 @@
 package CodeFights.Intro.Rainbow_of_Clarity;
 
+/*** 
+---Pergunta/ Question---
+Determine if the given character is a digit or not.
+
+Determine se o caractere dado é um dígito ou não.
+---Comentários/ Comments---
+Make an algorithm to determine if the character is a digit.
+
+Faça um algoritmo para determinar se o caracter é um digito.
+***/
+
 public class isDigit {
     public static void main(String[] args) {
         System.out.println(isDigit('-'));
