c0e4e7428a65c03c47a68b152e4eb7efe42225f4
vinicius3333 <35425928+vinicius3333@users.noreply.github.com>
2018-09-01 15:08:23 -0300

    Update chessKnight.java

--- Intro/Rainbow_of_Clarity/chessKnight.java
+++ Intro/Rainbow_of_Clarity/chessKnight.java
@@ -1,5 +1,22 @@
 package CodeFights.Intro.Rainbow_of_Clarity;
 
+/*** 
+---Pergunta/ Question---
+Given a position of a knight on the standard chessboard, find the number of different moves the knight can perform.
+The knight can move to a square that is two squares horizontally and one square vertically, or two squares vertically and one
+square horizontally away from it. The complete move therefore looks like the letter L. Check out the image below to see all 
+valid moves for a knight piece that is placed on one of the central squares.
+
+Dada a posição de um cavaleiro no tabuleiro de xadrez padrão, encontre o número de movimentos diferentes que o cavaleiro pode
+realizar. O cavalo pode mover-se para um quadrado de dois quadrados na horizontal e um quadrado na vertical, ou dois quadrados 
+na vertical quadrado horizontalmente longe dele. O movimento completo, portanto, parece com a letra L. Confira a imagem abaixo 
+para ver todos movimentos válidos para uma peça de cavaleiro colocada em uma das praças centrais.
+---Comentários/ Comments---  
+Make an algorithm to check how many houses the horse can reach.
+
+Faça um algoritmo para verificar quantas casas o cavalo consegue alcançar.
+***/
+
 public class chessKnight {
     public static void main(String[] args) {
         System.out.println(chessKnight("a1"));
