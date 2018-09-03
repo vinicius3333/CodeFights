b6afbe9eaf19538c76d65d047732d18112fa63ff
vinicius3333 <35425928+vinicius3333@users.noreply.github.com>
2018-09-03 19:24:02 -0300

    Update longestWord.java

--- Intro/Land_of_Logic/longestWord.java
+++ Intro/Land_of_Logic/longestWord.java
@@ -1,5 +1,16 @@
 package CodeFights.Intro.Land_of_Logic;
 
+/*** 
+---Pergunta/ Question---
+Define a word as a sequence of consecutive English letters. Find the longest word from the given string.
+
+Defina uma palavra como uma sequência de letras inglesas consecutivas. Encontre a palavra mais longa da string dada.
+---Comentários/ Comments---
+Make an algorithm to return the String with the largest size within a sentence.
+
+Faça um algoritmo para retornar a String com o maior tamanho dentro de uma frase.
+***/
+
 import java.util.Arrays;
 
 public class longestWord {
