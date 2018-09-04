90f5ac86324a32707ea403f8cf4899e01990168c
vinicius3333 <35425928+vinicius3333@users.noreply.github.com>
2018-09-04 16:16:56 -0300

    Update Valid_Time.java

--- Intro/Land_of_Logic/Valid_Time.java
+++ Intro/Land_of_Logic/Valid_Time.java
@@ -1,5 +1,16 @@
 package CodeFights.Intro.Land_of_Logic;
 
+/*** 
+---Pergunta/ Question---
+Check if the given string is a correct time representation of the 24-hour clock.
+
+Verifique se a string fornecida é uma representação de hora correta do relógio de 24 horas.
+---Comentários/ Comments---
+Make an algorithm to find out if the String is in the correct temp format.
+
+Faça um algoritmo para descobrir se a String está no formato de temp correto.
+***/
+
 public class Valid_Time {
     public static void main(String[] args) {
         System.out.println(validTime("25:51"));
