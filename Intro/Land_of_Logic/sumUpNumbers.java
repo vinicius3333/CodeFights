ca6d5d1f980ae77478f0b36c82f2fe43c9d4a9ab
vinicius3333 <35425928+vinicius3333@users.noreply.github.com>
2018-09-05 08:31:21 -0300

    Update sumUpNumbers.java

--- Intro/Land_of_Logic/sumUpNumbers.java
+++ Intro/Land_of_Logic/sumUpNumbers.java
@@ -1,5 +1,22 @@
 package CodeFights.Intro.Land_of_Logic;
 
+/*** 
+---Pergunta/ Question---
+CodeMaster has just returned from shopping. He scanned the check of the items he bought and gave the resulting string to Ratiorg to
+figure out the total number of purchased items. Since Ratiorg is a bot he is definitely going to automate it, so he needs a program
+that sums up all the numbers which appear in the given input. Help Ratiorg by writing a function that returns the sum of numbers that 
+appear in the given inputString.
+
+O CodeMaster acaba de retornar das compras. Ele escaneou o cheque dos itens que comprou e deu a string resultante para Ratiorg para
+descobrir o número total de itens comprados. Já que o Ratiorg é um bot ele definitivamente vai automatizá-lo, então ele precisa de um 
+programa que resume todos os números que aparecem na entrada dada. Ajude Ratiorg escrevendo uma função que retorne a soma dos números 
+que aparecem na entrada inputString.
+---Comentários/ Comments---
+Make an algorithm that adds all the digits of the given String.
+
+Faça um algoritmo que some todos os dígitos da String dada.
+***/
+
 import java.util.Arrays;
 
 public class sumUpNumbers {
