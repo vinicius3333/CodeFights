a7ecc96310741adba2568577cb58451ec7f8f710
vinicius3333 <35425928+vinicius3333@users.noreply.github.com>
2018-09-08 14:27:00 -0300

    Update digitsProduct.java

--- Intro/Land_of_Logic/digitsProduct.java
+++ Intro/Land_of_Logic/digitsProduct.java
@@ -1,5 +1,19 @@
 package CodeFights.Intro.Land_of_Logic;
 
+
+/*** 
+---Pergunta/ Question---
+Given an integer product, find the smallest positive (i.e. greater than 0) integer the product of whose digits is equal to 
+product. If there is no such integer, return -1 instead.
+
+Dado um produto inteiro, encontre o menor inteiro positivo (ou seja, maior que 0) cujo produto dos dígitos é igual ao produto. 
+Se não houver tal número inteiro, retorne -1.
+---Comentários/ Comments---
+Make an algorithm that finds a number that the product of its digits is equal to product variable.
+
+Faça um algoritmo que encontre um número que o produto dos digitos dele sejam iguais a variavel produto.
+***/
+
 public class digitsProduct {
     public static void main(String[] args) {
         System.out.println(digitsProduct(12));
