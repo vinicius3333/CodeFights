41849c9c59236366166a1aee311b4d6e74b3ba03
vinicius3333 <35425928+vinicius3333@users.noreply.github.com>
2018-09-09 15:20:11 -0300

    Update File_Naming.java

--- Intro/Land_of_Logic/File_Naming.java
+++ Intro/Land_of_Logic/File_Naming.java
@@ -1,5 +1,22 @@
 package CodeFights.Intro.Land_of_Logic;
 
+/*** 
+---Pergunta/ Question---
+You are given an array of desired filenames in the order of their creation. Since two files cannot have equal names, the one 
+which comes later will have an addition to its name in a form of (k), where k is the smallest positive integer such that the
+obtained name is not used yet. Return an array of names that will be given to the files.
+
+Você recebe uma variedade de nomes de arquivos desejados na ordem de sua criação. Como dois arquivos não podem ter nomes 
+iguais, o que vem depois terá um acréscimo ao seu nome em uma forma de (k), onde k é o menor inteiro positivo tal que o
+nome obtido ainda não é usado. Retorna uma matriz de nomes que serão dados aos arquivos.
+---Comentários/ Comments---
+Make an algorithm that, if the array has one or more Strings equal, put "(k)" after the String, where k is the smallest positive 
+integer, so that the name is not yet used.
+
+Faça um algoritmo que, se a array tiver uma ou mais Strings iguais, coloque "(k)" depois da String, onde k é o menor inteiro
+positivo, de forma que o nome obtido ainda não seja usado.
+***/
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
