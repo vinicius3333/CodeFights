f8e353740e05981c35053ac54133459335ef0f45
vinicius3333 <35425928+vinicius3333@users.noreply.github.com>
2018-09-10 16:56:05 -0300

    Update messageFromBinaryCode.java

--- Intro/Land_of_Logic/messageFromBinaryCode.java
+++ Intro/Land_of_Logic/messageFromBinaryCode.java
@@ -1,5 +1,22 @@
 package CodeFights.Intro.Land_of_Logic;
 
+/*** 
+---Pergunta/ Question---
+You are taking part in an Escape Room challenge designed specifically for programmers. In your efforts to find a clue, you've
+found a binary code written on the wall behind a vase, and realized that it must be an encrypted message. After some 
+thought, your first guess is that each consecutive 8 bits of the code stand for the character with the corresponding extended 
+ASCII code. Assuming that your hunch is correct, decode the message.
+
+Você está participando de um desafio do Escape Room projetado especificamente para programadores. Em seus esforços para
+encontrar uma pista, você encontra um código binário escrito na parede atrás de um vaso e percebe que ele deve ser uma 
+mensagem criptografada. Após alguns pensamentos, seu primeiro palpite é que cada 8 bits consecutivos do código representam o 
+caractere com a extensão estendida correspondente. Código ASCII. Supondo que seu palpite esteja correto, decodifique a mensagem.
+---Comentários/ Comments---
+Make an algorithm that takes the binary codes and transforms them into characters through the ASCII table.
+
+Faça um algoritmo que pegue os códigos binários e transforme-os em caracteres através da tabela ASCII.
+***/
+
 public class messageFromBinaryCode {
     public static void main(String[] args) {
 
