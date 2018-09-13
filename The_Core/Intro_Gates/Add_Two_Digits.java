9f16077bd353674111d97623b26ed844eecbd86d
vinicius3333 <35425928+vinicius3333@users.noreply.github.com>
2018-09-13 15:02:23 -0300

    Update Add_Two_Digits.java

--- The_Core/Intro_Gates/Add_Two_Digits.java
+++ The_Core/Intro_Gates/Add_Two_Digits.java
@@ -1,5 +1,16 @@
 package CodeFights.The_Core.Intro_Gates;
 
+/*** 
+---Pergunta/ Question---
+You are given a two-digit integer n. Return the sum of its digits.
+
+Você recebe um inteiro de dois dígitos n. Retorna a soma de seus dígitos.
+---Comentários/ Comments---
+Make an algorithm that returns the sum of the two digits of a number.
+
+Faça um algoritmo que retorne a soma dos dois dígitos de um número.
+***/
+
 public class Add_Two_Digits {
     public static void main(String[] args){
         System.out.println(addTwoDigits(24));
