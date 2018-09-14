f636b2793417b81b6d238dd3009b51edc4b8a589
vinicius3333 <35425928+vinicius3333@users.noreply.github.com>
2018-09-14 08:41:56 -0300

    Update Largest_Number.java

--- The_Core/Intro_Gates/Largest_Number.java
+++ The_Core/Intro_Gates/Largest_Number.java
@@ -1,5 +1,16 @@
 package CodeFights.The_Core.Intro_Gates;
 
+/*** 
+---Pergunta/ Question---
+Given an integer n, return the largest number that contains exactly n digits.
+
+Dado um inteiro n, retorna o maior número que contém exatamente n dígitos.
+---Comentários/ Comments---
+Make an algorithm that returns as many n-digits as possible.
+
+Faça um algoritmo que retorne o maior número possível com a quantiade de n dígitos.
+***/
+
 public class Largest_Number {
     public static void main(String[] args) {
         System.out.println(largestNumber(3));
