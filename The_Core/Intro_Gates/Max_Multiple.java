cb6dac4855f111c0a0341b57fd79502195ebb1bc
vinicius3333 <35425928+vinicius3333@users.noreply.github.com>
2018-09-17 16:23:23 -0300

    Update Max_Multiple.java

--- The_Core/Intro_Gates/Max_Multiple.java
+++ The_Core/Intro_Gates/Max_Multiple.java
@@ -1,5 +1,24 @@
 package CodeFights.The_Core.Intro_Gates;
 
+/*** 
+---Pergunta/ Question---
+Given a divisor and a bound, find the largest integer N such that:
+    N is divisible by divisor.
+    N is less than or equal to bound.
+    N is greater than 0.
+It is guaranteed that such a number exists.
+
+Dado um divisor e um limite, encontre o maior inteiro N tal que:
+     N é divisível por divisor.
+     N é menor ou igual a limite.
+     N é maior que 0.
+É garantido que esse número existe.
+---Comentários/ Comments---
+Make an algorithm that returns the largest number divisible by the "divisor" variable and does not pass the "bound" variable number.
+
+Faça um algoritmo que retorne o maior número divisivel pela variável "divisor" e que não passe o número da variável "bound".
+***/
+
 public class Add_Two_Digits {
     public static void main(String[] args){
         System.out.println(maxMultiple(3,10));
