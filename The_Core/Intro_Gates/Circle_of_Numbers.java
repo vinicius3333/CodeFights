88e5d6b780a653aa5b09d1c35a0a430d914918cc
vinicius3333 <35425928+vinicius3333@users.noreply.github.com>
2018-09-18 13:17:20 -0300

    Update Circle_of_Numbers.java

--- The_Core/Intro_Gates/Circle_of_Numbers.java
+++ The_Core/Intro_Gates/Circle_of_Numbers.java
@@ -1,4 +1,18 @@
 package CodeFights.The_Core.Intro_Gates;
+/*** 
+---Pergunta/ Question---
+Consider integer numbers from 0 to n - 1 written down along the circle in such a way that the distance between any two neighboring 
+numbers is equal (note that 0 and n - 1 are neighboring, too). Given n and firstNumber, find the number which is written in the
+radially opposite position to firstNumber.
+
+Considere números inteiros de 0 a n - 1 escritos ao longo do círculo de tal maneira que a distância entre quaisquer dois vizinhos
+números são iguais (note que 0 e n - 1 são vizinhos também). Dado n e firstNumber, encontre o número que está escrito no
+posição radialmente oposta ao firstNumber.
+---Comentários/ Comments---
+Make an algorithm that finds the number radially opposite the number of the firstNumber.
+
+Faça um algoritmo que encontre o número radialmente oposto ao número da váriavel firstNumber.
+***/
 
 public class Circle_of_Numbers {
     public static void main(String[] args) {
