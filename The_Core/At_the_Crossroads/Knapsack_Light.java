a6e7c41cbd6199389c6011c20c233e3c67451277
vinicius3333 <35425928+vinicius3333@users.noreply.github.com>
2018-09-23 12:39:42 -0300

    Update Knapsack_Light.java

--- The_Core/At_the_Crossroads/Knapsack_Light.java
+++ The_Core/At_the_Crossroads/Knapsack_Light.java
@@ -1,8 +1,25 @@
 package CodeFights.The_Core.At_the_Crossroads;
 
+/*** 
+---Pergunta/ Question---
+You found two items in a treasure chest! The first item weighs weight1 and is worth value1, and the second item weighs weight2 and
+is worth value2. What is the total maximum value of the items you can take with you, assuming that your max weight capacity is 
+maxW and you can't come back for the items later? Note that there are only two items and you can't bring more than one item of 
+each type, i.e. you can't take two first items or two second items.
+
+Você encontrou dois itens em um baú de tesouro! O primeiro item pesa peso1 e vale a pena valor1, e o segundo item pesa peso2 e
+vale a pena value2. Qual é o valor máximo total dos itens que você pode levar com você, supondo que sua capacidade máxima de peso
+seja maxW e você não pode voltar para os itens mais tarde? Note que existem apenas dois itens e você não pode trazer mais de um
+item cada tipo, ou seja, você não pode levar dois primeiros itens ou dois segundos itens.
+---Comentários/ Comments---
+Make an algorithm to determine how much weight you can carry.
+
+Faça um algoritmo para determinar quanto de peso você consegue carregar.
+***/
+
 public class Knapsack_Light {
     public static void main(String[] args){
-
+        System.out.println(knapsackLight(3, 6, 13, 4, 19));
     }
 
     static int knapsackLight(int value1, int weight1, int value2, int weight2, int maxW) {
