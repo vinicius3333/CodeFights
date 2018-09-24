9a8e7a265de938e1fcd01bddf5dabd8f20130526
vinicius3333 <35425928+vinicius3333@users.noreply.github.com>
2018-09-24 09:16:51 -0300

    Update Extra_Number.java

--- The_Core/At_the_Crossroads/Extra_Number.java
+++ The_Core/At_the_Crossroads/Extra_Number.java
@@ -1,5 +1,18 @@
 package CodeFights.The_Core.At_the_Crossroads;
 
+/*** 
+---Pergunta/ Question---
+You're given three integers, a, b and c. It is guaranteed that two of these integers are equal to each other. What is the value of the
+third integer?
+
+Você recebe três inteiros, a, bec. É garantido que dois desses números inteiros são iguais entre si. Qual é o valor do
+terceiro inteiro?
+---Comentários/ Comments---
+Make an algorithm that returns between the three numbers, which is similar to no other.
+
+Faça um algoritmo que retorne entre os três números, o qual não é semelhante a nenhum outro.
+***/
+
 public class Extra_Number {
     public static void main(String[] args){
         System.out.println(extraNumber(2, 7, 2));
