3be61b0ae0f7e8ba18cfecb8bd85b35acb15207b
vinicius3333 <35425928+vinicius3333@users.noreply.github.com>
2018-09-27 16:18:41 -0300

    Update Tennis_Set.java

--- The_Core/At_the_Crossroads/Tennis_Set.java
+++ The_Core/At_the_Crossroads/Tennis_Set.java
@@ -1,5 +1,20 @@
 package CodeFights.The_Core.At_the_Crossroads;
 
+/*** 
+---Pergunta/ Question---
+In tennis, a set is finished when one of the players wins 6 games and the other one wins less than 5, or, if both players win at
+least 5 games, until one of the players wins 7 games. Determine if it is possible for a tennis set to be finished with the score 
+score1 : score2.
+
+No ténis, um conjunto termina quando um dos jogadores ganha 6 jogos e o outro ganha menos de 5, ou, se ambos os jogadores ganharem
+em menos 5 jogos, até que um dos jogadores ganhe 7 jogos. Determine se é possível terminar um jogo de tênis com a pontuação
+score1: score2.
+---Comentários/ Comments---
+Make an algorithm to check if it is possible for the game of tennis to end with scoreboard score1: score2.
+
+Faça um algoritmo para verificar se é possível para o jogo de tennis acabar com o placar score1 : score2.
+***/
+
 public class Tennis_Set {
     public static void main(String[] args) {
         System.out.println(tennisSet(3, 6));
