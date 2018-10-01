09421884abfb9dbe1ec85af323bd62dc17313739
vinicius3333 <35425928+vinicius3333@users.noreply.github.com>
2018-10-01 09:07:56 -0300

    Update Array_Packing.java

--- The_Core/Corner_of_0s_and_1s/Array_Packing.java
+++ The_Core/Corner_of_0s_and_1s/Array_Packing.java
@@ -1,5 +1,28 @@
 package CodeFights.The_Core.Corner_of_0s_and_1s;
 
+/*** 
+---Pergunta/ Question---
+You are given an array of up to four non-negative integers, each less than 256.
+Your task is to pack these integers into one number M in the following way:
+The first element of the array occupies the first 8 bits of M;
+The second element occupies next 8 bits, and so on.
+Return the obtained integer M.
+
+Você recebe uma matriz de até quatro inteiros não negativos, cada um com menos de 256.
+
+Sua tarefa é empacotar esses números inteiros em um número M da seguinte maneira:
+
+O primeiro elemento do array ocupa os primeiros 8 bits de M;
+O segundo elemento ocupa os próximos 8 bits e assim por diante.
+Retornar o inteiro obtido M.
+---Comentários/ Comments---
+Make an algorithm that takes all the numbers in an array, converts them to binary numbers, and then joins them and converts them back 
+to decimal places.
+
+Faça um algoritmo que pegue todos os números de uma array, converta-os para números binários e depois junte-os e converta novamente para
+casa decimal.
+***/
+
 public class Array_Packing {
     public static void main(String[] args) {
         System.out.println(arrayPacking(new int[]{24, 85, 0}));
