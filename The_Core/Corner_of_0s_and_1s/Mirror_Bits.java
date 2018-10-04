a2b0460a64fc1af7f1f282022c682aefb886e9c9
vinicius3333 <35425928+vinicius3333@users.noreply.github.com>
2018-10-03 23:11:37 -0300

    Update Mirror_Bits.java

--- The_Core/Corner_of_0s_and_1s/Mirror_Bits.java
+++ The_Core/Corner_of_0s_and_1s/Mirror_Bits.java
@@ -1,5 +1,16 @@
 package CodeFights.The_Core.Corner_of_0s_and_1s;
 
+/*** 
+---Pergunta/ Question---
+Reverse the order of the bits in a given integer.
+
+Inverta a ordem dos bits de um número inteiro.
+---Comentários/ Comments---
+Make an algorithm that converts the bits of the integer number.
+
+Faça um algoritmo que converta os bits do numéro inteiro.
+***/
+
 public class Mirror_Bits {
     public static void main(String[] args) {
         System.out.println(mirrorBits(97));
