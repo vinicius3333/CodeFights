a18282d12d19c367c4ce54f24d8bbd9122e0017e
vinicius3333 <35425928+vinicius3333@users.noreply.github.com>
2018-10-10 08:12:22 -0300

    Update Least_Factorial.java

--- The_Core/Loop_Tunnel/Least_Factorial.java
+++ The_Core/Loop_Tunnel/Least_Factorial.java
@@ -1,5 +1,22 @@
 package CodeFights.The_Core.Loop_Tunnel;
 
+/*** 
+---Pergunta/ Question---
+Given an integer n, find the minimal k such that
+k = m! (where m! = 1 * 2 * ... * m) for some integer m;
+k >= n.
+In other words, find the smallest factorial which is not less than n.
+
+Dado um inteiro n, encontre o mínimo k tal que
+k = m! (onde m! = 1 * 2 * ... * m) para algum inteiro m;
+k> = n.
+Em outras palavras, encontre o menor fatorial que não seja menor que n.
+---Comentários/ Comments---
+Make an algorithm to find the smallest factorial greater than n.
+
+Faça um algoritmo para encontrar o menor fatorial maior que n.
+***/
+
 public class Least_Factorial {
     public static void main(String[] args) {
         System.out.println(leastFactorial(17));
