ccb6ef3f649d8f0d343e845015d635e1d03b512f
vinicius3333 <35425928+vinicius3333@users.noreply.github.com>
2018-10-11 08:10:07 -0300

    Update Count_Sum_of_Two_Representations_2.java

--- The_Core/Loop_Tunnel/Count_Sum_of_Two_Representations_2.java
+++ The_Core/Loop_Tunnel/Count_Sum_of_Two_Representations_2.java
@@ -1,5 +1,16 @@
 package CodeFights.The_Core.Loop_Tunnel;
 
+/*** 
+---Pergunta/ Question---
+Given integers n, l and r, find the number of ways to represent n as a sum of two integers A and B such that l ≤ A ≤ B ≤ r.
+
+Dado inteiros n, le r, ache o número de modos para representar n como uma soma de dois inteiros A e B tal que l ≤ A ≤ B ≤ r.
+---Comentários/ Comments---
+Make an algorithm to show how many times you can add two numbers and result in n.
+
+Faça um algoritmo para mostrar quantas vezes se consegue somar dois números e resultar em n.
+***/
+
 public class Count_Sum_of_Two_Representations_2 {
     public static void main(String[] args) {
         System.out.println(countSumOfTwoRepresentations2(6, 2, 4));
