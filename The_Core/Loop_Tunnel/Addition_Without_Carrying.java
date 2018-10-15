4a054488e68e1548536634510fe156beb4634dc9
vinicius3333 <35425928+vinicius3333@users.noreply.github.com>
2018-10-15 18:31:54 -0300

    Update Addition_Without_Carrying.java

--- The_Core/Loop_Tunnel/Addition_Without_Carrying.java
+++ The_Core/Loop_Tunnel/Addition_Without_Carrying.java
@@ -1,5 +1,19 @@
 package CodeFights.The_Core.Loop_Tunnel;
 
+/*** 
+---Pergunta/ Question---
+A little boy is studying arithmetics. He has just learned how to add two integers, written one below another, column by column.
+But he always forgets about the important part - carrying. Given two integers, find the result which the little boy will get.
+
+Um menino está estudando aritmética. Ele acabou de aprender como adicionar dois inteiros, escritos um abaixo do outro, coluna
+por coluna. Mas ele sempre se esquece da parte importante - carregando. Dados dois números inteiros, encontre o resultado que o
+menino receberá.
+---Comentários/ Comments---
+Make an algorithm to add numbers, but only showing the last digit on the right.
+
+Faça um algoritmo para somar números, porém apenas mostrando o último dígito do direita.
+***/
+
 public class Addition_Without_Carrying {
     public static void main(String[] args) {
         System.out.println(additionWithoutCarrying(456, 1734));
