e04788c288e9d9f77bb7a3dca869af049dc0d23c
vinicius3333 <35425928+vinicius3333@users.noreply.github.com>
2018-10-16 18:11:35 -0300

    Update Apple_Boxes.java

--- The_Core/Loop_Tunnel/Apple_Boxes.java
+++ The_Core/Loop_Tunnel/Apple_Boxes.java
@@ -1,5 +1,24 @@
 package CodeFights.The_Core.Loop_Tunnel;
 
+/*** 
+---Pergunta/ Question---
+You have k apple boxes full of apples. Each square box of size m contains m × m apples. You just noticed two interesting 
+properties about the boxes:
+The smallest box is size 1, the next one is size 2,..., all the way up to size k.
+Boxes that have an odd size contain only yellow apples. Boxes that have an even size contain only red apples.
+Your task is to calculate the difference between the number of red apples and the number of yellow apples.
+
+Você tem caixas de maçã k cheias de maçãs. Cada caixa quadrada de tamanho m contém maçãs m × m. Você acabou de notar duas
+propriedades interessantes sobre as caixas:
+A menor caixa é o tamanho 1, a próxima é o tamanho 2, ... até o tamanho k.
+As caixas que têm um tamanho ímpar contêm apenas maçãs amarelas. Caixas que têm um tamanho par contêm apenas maçãs vermelhas.
+Sua tarefa é calcular a diferença entre o número de maçãs vermelhas e o número de maçãs amarelas.
+---Comentários/ Comments---
+Make an algorithm to return the difference of yellow apples and red apples.
+
+Faça um algoritmo para retornar a diferença de maçãs amarelas e maçãs vermelhas.
+***/
+
 public class Apple_Boxes {
     public static void main(String[] args) {
         System.out.println(appleBoxes(5));
