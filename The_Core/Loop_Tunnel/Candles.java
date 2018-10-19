cf4a9d8c09a43a8a24e6eda8687e8e34cbbd6589
vinicius3333 <35425928+vinicius3333@users.noreply.github.com>
2018-10-19 16:29:19 -0300

    Update Candles.java

--- The_Core/Loop_Tunnel/Candles.java
+++ The_Core/Loop_Tunnel/Candles.java
@@ -1,5 +1,22 @@
 package CodeFights.The_Core.Loop_Tunnel;
 
+/*** 
+---Pergunta/ Question---
+When a candle finishes burning it leaves a leftover. makeNew leftovers can be combined to make a new candle, which, when burning 
+down, will in turn leave another leftover.
+You have candlesNumber candles in your possession. What's the total number of candles you can burn, assuming that you create new
+candles as soon as you have enough leftovers?
+
+Quando uma vela termina de queimar deixa uma sobra. makeNew sobras podem ser combinadas para fazer uma nova vela, que, ao 
+queimar, por sua vez, deixará outra sobra.
+Você tem candlesNumber velas em sua posse. Qual é o número total de velas que você pode gravar, presumindo que você crie novas
+velas assim que tiver sobras suficientes?
+---Comentários/ Comments---
+Make an algorithm that returns how many candles are possible with the candles already made and with the leftovers.
+
+Faça um algoritmo que retorne quantas velas são possíveis fazer com as velas já feitas e com as sobras.
+***/
+
 public class Candles {
     public static void main(String[] args){
         System.out.println(candles(5,2));
