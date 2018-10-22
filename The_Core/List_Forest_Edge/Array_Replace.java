5eb689c2dfe4cfa0ff6c462c2eb65bc0731f3b04
vinicius3333 <35425928+vinicius3333@users.noreply.github.com>
2018-10-22 20:50:17 -0300

    Update Array_Replace.java

--- The_Core/List_Forest_Edge/Array_Replace.java
+++ The_Core/List_Forest_Edge/Array_Replace.java
@@ -1,5 +1,16 @@
 package CodeFights.The_Core.List_Forest_Edge;
 
+/*** 
+---Pergunta/ Question---
+Given an array of integers, replace all the occurrences of elemToReplace with substitutionElem.
+
+Dado um array de inteiros, substitua todas as ocorrências de elemToReplace por substitutionElem.
+---Comentários/ Comments---
+Make an algorithm that returns the algorithm with the values exchanged.
+
+Faça um algoritmo que retorne o algoritmo com os valores trocados.
+***/
+
 public class Array_Replace {
 
     public static void main(String[] args) {
