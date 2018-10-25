5e8eaeb01ed8c9b79cb43060869c6984ac2d62de
vinicius3333 <35425928+vinicius3333@users.noreply.github.com>
2018-10-25 14:27:22 -0300

    Update Concatenate_Arrays.java

--- The_Core/List_Forest_Edge/Concatenate_Arrays.java
+++ The_Core/List_Forest_Edge/Concatenate_Arrays.java
@@ -1,5 +1,16 @@
 package CodeFights.The_Core.List_Forest_Edge;
 
+/*** 
+---Pergunta/ Question---
+Given two arrays of integers a and b, obtain the array formed by the elements of a followed by the elements of b.
+
+Dadas duas matrizes de inteiros aeb, obter o array formado pelos elementos de a seguido pelos elementos de b.
+---Comentários/ Comments---
+Make an algorithm that returns the join of the two arrays.
+
+Faça um algoritmo que retorne a junção das duas arrays.
+***/
+
 public class Concatenate_Arrays {
     public static void main(String[] args){
 
