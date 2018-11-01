5238846a6fd1c11acb56b71c137d4cd5b1f6db85
vinicius3333 <35425928+vinicius3333@users.noreply.github.com>
2018-11-01 08:06:32 -0300

    Update Is_Power.java

--- The_Core/Labyrinth_of_Nested_Loops/Is_Power.java
+++ The_Core/Labyrinth_of_Nested_Loops/Is_Power.java
@@ -1,5 +1,16 @@
 package CodeFights.The_Core.Labyrinth_of_Nested_Loops;
 
+/*** 
+---Pergunta/ Question---
+Determine if the given number is a power of some non-negative integer.
+
+Determine se o número fornecido é uma potência de um número inteiro não negativo.
+---Comentários/ Comments---
+Make an algorithm that returns if the number is the result of some exponential number.
+
+Faça um algoritmo que retorne se o número é o resultado de algum número exponencial.
+***/
+
 public class Is_Power {
     public static void main(String[] args){
         System.out.println(isPower(125));
