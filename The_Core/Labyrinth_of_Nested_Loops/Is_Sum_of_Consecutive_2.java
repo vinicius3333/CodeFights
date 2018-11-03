c13d7b91a55b17d87861762e24332372fb56bcbc
vinicius3333 <35425928+vinicius3333@users.noreply.github.com>
2018-11-02 21:09:32 -0300

    Update Is_Sum_of_Consecutive_2.java

--- The_Core/Labyrinth_of_Nested_Loops/Is_Sum_of_Consecutive_2.java
+++ The_Core/Labyrinth_of_Nested_Loops/Is_Sum_of_Consecutive_2.java
@@ -1,5 +1,16 @@
 package CodeFights.The_Core.Labyrinth_of_Nested_Loops;
 
+/*** 
+---Pergunta/ Question---
+Find the number of ways to express n as sum of some (at least two) consecutive positive integers.
+
+Encontre o número de maneiras de expressar n como a soma de alguns (pelo menos dois) inteiros positivos consecutivos.
+---Comentários/ Comments---
+Make an algorithm to return how many times it is possible to make the number with the sum of other numbers.
+
+Faça um algoritmo para retorne quantas vezes é possível fazer o número com a soma de outros números.
+***/
+
 public class Is_Sum_of_Consecutive_2 {
     public static void main(String[] args) {
         System.out.println(isSumOfConsecutive2(9));
