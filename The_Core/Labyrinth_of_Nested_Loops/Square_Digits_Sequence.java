e291eb5ba1f2a0f0db2982bdf587735d68440bc4
vinicius3333 <35425928+vinicius3333@users.noreply.github.com>
2018-11-03 22:43:29 -0300

    Update Square_Digits_Sequence.java

--- The_Core/Labyrinth_of_Nested_Loops/Square_Digits_Sequence.java
+++ The_Core/Labyrinth_of_Nested_Loops/Square_Digits_Sequence.java
@@ -1,5 +1,20 @@
 package CodeFights.The_Core.Labyrinth_of_Nested_Loops;
 
+/*** 
+---Pergunta/ Question---
+Consider a sequence of numbers a0, a1, ..., an, in which an element is equal to the sum of squared digits of the previous
+element. The sequence ends once an element that has already been in the sequence appears again. Given the first element a0, find 
+the length of the sequence.
+
+Considere uma sequência de números a0, a1, ..., an, em que um elemento é igual à soma dos dígitos quadrados do elemento anterior.
+A sequência termina quando um elemento que já está na seqüência aparece novamente. Dado o primeiro elemento a0, encontre o
+comprimento da sequência.
+---Comentários/ Comments---
+Make an algorithm to find the sequence length of the number a0.
+
+Faça um algoritmo para descobrir o tamanho da sequência do número a0.
+***/
+
 import java.util.ArrayList;
 
 public class Square_Digits_Sequence {
