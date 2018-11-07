ffe341be5c35cb89639f217fdf1e7b107c988ec7
vinicius3333 <35425928+vinicius3333@users.noreply.github.com>
2018-11-07 11:30:29 -0200

    Update Weak_Numbers.java

--- The_Core/Labyrinth_of_Nested_Loops/Weak_Numbers.java
+++ The_Core/Labyrinth_of_Nested_Loops/Weak_Numbers.java
@@ -1,5 +1,26 @@
 package CodeFights.The_Core.Labyrinth_of_Nested_Loops;
 
+/*** 
+---Pergunta/ Question---
+We define the weakness of number x as the number of positive integers smaller than x that have more divisors than x.
+It follows that the weaker the number, the greater overall weakness it has. For the given integer n, you need to answer two questions:
+what is the weakness of the weakest numbers in the range [1, n]?
+how many numbers in the range [1, n] have this weakness?
+Return the answer as an array of two elements, where the first element is the answer to the first question, and the second element is 
+the answer to the second question.
+
+Nós definimos a fraqueza do número x como o número de inteiros positivos menores que x que têm mais divisores que x.
+Segue-se que quanto mais fraco o número, maior a fraqueza geral que ele tem. Para o inteiro dado n, você precisa responder duas perguntas:
+Qual é a fraqueza dos números mais fracos no intervalo [1, n]?
+Quantos números no intervalo [1, n] têm essa fraqueza?
+Retorna a resposta como um array de dois elementos, onde o primeiro elemento é a resposta para a primeira pergunta, e o segundo elemento 
+é a resposta para a segunda questão.
+---Comentários/ Comments---
+Make an algorithm that returns an Array with the weakest numbers.
+
+Faça um algoritmo que retorna uma Array com os números mais fracos.
+***/
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.stream.Collectors;
