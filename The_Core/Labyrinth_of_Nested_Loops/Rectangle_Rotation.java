737d2f14d744e73f55abc0d4085fa357fbc508d7
vinicius3333 <35425928+vinicius3333@users.noreply.github.com>
2018-11-10 18:32:01 -0200

    Update Rectangle_Rotation.java

--- The_Core/Labyrinth_of_Nested_Loops/Rectangle_Rotation.java
+++ The_Core/Labyrinth_of_Nested_Loops/Rectangle_Rotation.java
@@ -1,5 +1,22 @@
 package CodeFights.The_Core.Labyrinth_of_Nested_Loops;
 
+/*** 
+---Pergunta/ Question---
+A rectangle with sides equal to even integers a and b is drawn on the Cartesian plane. Its center (the intersection point of its
+diagonals) coincides with the point (0, 0), but the sides of the rectangle are not parallel to the axes; instead, they are forming
+45 degree angles with the axes.
+How many points with integer coordinates are located inside the given rectangle (including on its sides)?
+
+Um retângulo com lados iguais aos números inteiros aeb é desenhado no plano cartesiano. Seu centro (o ponto de interseção de suas
+diagonais) coincide com o ponto (0, 0), mas os lados do retângulo não são paralelos aos eixos; em vez disso, eles estão formando
+ângulos de 45 graus com os eixos.
+Quantos pontos com coordenadas inteiras estão localizados dentro do retângulo dado (incluindo nos lados)?
+---Comentários/ Comments---
+Make an algorithm to count the green points of the plane.
+
+Faça um algoritmo para contar os pontos verdes do plano.
+***/
+
 public class Rectangle_Rotation {
     public static void main(String[] args){
         System.out.println(rectangleRotation(6,4));
